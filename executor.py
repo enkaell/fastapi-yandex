cab7283c4c6df47fd55c6c23623a4de6f3304cda
vlad <vlad2012chesnokov@mail.ru>
2022-08-31 21:18:28 +0500

    xml

--- executor.py
+++ executor.py
@@ -1,11 +1,13 @@
-import datetime
-
 from openpyxl import load_workbook
 import requests
 from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
-import xml.etree.ElementTree as ET
-
+import time
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+import uvicorn
+import datetime
+from dataclasses import dataclass
 path = 'yandex.xlsx'
 
 
@@ -13,32 +15,33 @@ def update_yandex_table():
     json_data = {"password": "RAMTRX1500", "regulation": True, "email": "Rakhmanov-2019@list.ru"}
     response = requests.post('https://www.sima-land.ru/api/v5/signin', json=json_data)
     token = response.json().get('token')
-    tree = ET.parse('yandex.xml')
-    root_node = tree.getroot()
+    wb = load_workbook('yandex.xlsx')
+    sheet = wb['Остатки']
     session = requests.Session()
     retry = Retry(connect=3, backoff_factor=0.5)
     adapter = HTTPAdapter(max_retries=retry)
     session.mount('https://', adapter)
-    for tag in root_node.findall('shop/offers/offer'):
-        try:
-            response = session.get(
-                f"https://www.sima-land.ru/api/v5/item/{tag.attrib['id']}",
-                headers={
-                    'accept': 'application/json',
-                    'X-Api-Key': token,
-                    'Authorization': token,
-                },
 
-                params={
-                    'view': 'brief',
-                    'by_sid': 'false',
-                }
-            )
-        except Exception as e:
-            tree.write('ostatki.xml', encoding='utf-8')
-        tag.find('count').text = str(response.json()['balance'])
+    start_time = int(time.time())
+    for i in sheet['C'][3:]:
+        response = session.get(
+            f'https://www.sima-land.ru/api/v5/item/{i.value}',
+            headers={
+                'accept': 'application/json',
+                'X-Api-Key': token,
+                'Authorization': token,
+            },
+
+            params={
+                'view': 'brief',
+                'by_sid': 'false',
+            },
+            timeout=(6, 9)
+        )
+        sheet['E'][i.column].value = response.json()['balance']
         print(response.json()['sid'], " обновлен")
-    tree.write('ostatki.xml', encoding='utf-8')
+
+    wb.save('yandex.xlsx')
 
 
 update_yandex_table()
