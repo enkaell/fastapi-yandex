c212ab02379094dc8a8de450f120d85ff3447126
vlad <vlad2012chesnokov@mail.ru>
2022-09-02 19:09:10 +0500

    return zip

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-from openpyxl import load_workbook
 import requests
 from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
-import time
-from fastapi import FastAPI
+import zipfile
+from fastapi import FastAPI, BackgroundTasks
 from fastapi.responses import FileResponse
 import uvicorn
 import datetime
 from dataclasses import dataclass
-
+import xml.etree.ElementTree as ET
+import zlib, sys
 
 @dataclass
 class Date:
@@ -16,7 +16,7 @@ class Date:
     info: str
 
 
-path = 'ezyzip.zip'
+path = 'ostatki.zip'
 app = FastAPI()
 Token = 123
 
@@ -30,40 +30,50 @@ def update_yandex_table():
     json_data = {"password": "RAMTRX1500", "regulation": True, "email": "Rakhmanov-2019@list.ru"}
     response = requests.post('https://www.sima-land.ru/api/v5/signin', json=json_data)
     token = response.json().get('token')
-    wb = load_workbook('yandex.xlsx')
-    sheet = wb['Остатки']
+    tree = ET.parse('yandex.xml')
+    root_node = tree.getroot()
     session = requests.Session()
     retry = Retry(connect=2, backoff_factor=0.5)
     adapter = HTTPAdapter(max_retries=retry)
     session.mount('https://', adapter)
+    for tag in root_node.findall('shop/offers/offer'):
+        try:
+            tag.remove(tag[1])
+        except Exception:
+            pass
+        try:
+            response = session.get(
+                f"https://www.sima-land.ru/api/v5/item/{tag.attrib['id']}",
+                headers={
+                    'accept': 'application/json',
+                    'X-Api-Key': token,
+                    'Authorization': token,
+                },
 
-    start_time = int(time.time())
-    for i in sheet['C'][3:]:
-        response = session.get(
-            f'https://www.sima-land.ru/api/v5/item/{i.value}',
-            headers={
-                'accept': 'application/json',
-                'X-Api-Key': token,
-                'Authorization': token,
-            },
-
-            params={
-                'view': 'brief',
-                'by_sid': 'false',
-            },
-            timeout=(6, 9)
-        )
-        sheet['E'][i.column].value = response.json()['balance']
+                params={
+                    'view': 'brief',
+                    'by_sid': 'false',
+                }
+            )
+        except Exception as e:
+            tree.write('ostatki.xml', encoding='utf-8')
+            zf = zipfile.ZipFile("ostatki.zip", "w", compresslevel=8, compression=zipfile.ZIP_DEFLATED)
+            zf.write('t.xml', compresslevel=8)
+        if int(tag.find('count').text) < 10:
+            tag.find('count').text = '0'
+        else:
+            tag.find('count').text = str(response.json()['balance'])
         print(response.json()['sid'], " обновлен")
-
-    wb.save('yandex.xlsx')
+    tree.write('ostatki.xml', encoding='utf-8')
+    zf = zipfile.ZipFile("ostatki.zip", "w", compresslevel=8, compression=zipfile.ZIP_DEFLATED)
+    zf.write('t.xml', compresslevel=8)
     Date.date = str(datetime.datetime.now())
     Date.info = "Ended"
 
 
-@app.get("/start", response_class=FileResponse)
-async def start():
-    update_yandex_table()
+@app.get("/start")
+async def start(background_tasks: BackgroundTasks):
+    background_tasks.add_task(update_yandex_table)
     Date.date = str(datetime.datetime.now())
     Date.info = "Started"
     return f"Started at {datetime.datetime.now()}"
