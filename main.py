8c8aa0604810232d91b263f521701ace736a743e
vlad <vlad2012chesnokov@mail.ru>
2022-09-05 19:39:40 +0500

    return zip

--- main.py
+++ main.py
@@ -9,13 +9,6 @@ import datetime
 from dataclasses import dataclass
 import xml.etree.ElementTree as ET
 
-
-@dataclass
-class Date:
-    date: str
-    info: str
-
-
 path = 'ostatki.zip'
 app = FastAPI()
 Token = 123
@@ -32,21 +25,37 @@ def update_yandex_table():
     token = response.json().get('token')
     zip = zipfile.ZipFile('ostatki.zip')
     zip.extractall()
-    Date.date = str(datetime.datetime.now())
-    Date.info = "Ended"
+    tree = ET.parse('t.xml')
+    root_node = tree.getroot()
+    session = requests.Session()
+    retry = Retry(connect=2, backoff_factor=0.5)
+    adapter = HTTPAdapter(max_retries=retry)
+    session.mount('https://', adapter)
+    for tag in root_node.findall('shop/offers/offer'):
+        response = session.get(
+            f"https://www.sima-land.ru/api/v5/item/{tag.attrib['id']}",
+            headers={
+                'accept': 'application/json',
+                'X-Api-Key': token,
+                'Authorization': token,
+            },
+
+            params={
+                'view': 'brief',
+                'by_sid': 'false',
+            }
+        )
+        if int(tag.find('count').text) < 10:
+            tag.find('count').text = '0'
+        else:
+            tag.find('count').text = str(response.json()['balance'])
+    tree.write('t.xml', encoding='utf-8')
 
 
 @app.get("/start")
 async def start(background_tasks: BackgroundTasks):
     background_tasks.add_task(update_yandex_table)
-    Date.date = str(datetime.datetime.now())
-    Date.info = "Started"
     return f"Started at {datetime.datetime.now()}"
-
-
-@app.get("/info")
-async def get_info():
-    return f"{Date.info} at {Date.date}"
 
 
 if __name__ == '__main__':
