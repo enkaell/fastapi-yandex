65b1cca98f71cb7ae9cbba865ca56f564723958f
vlad <vlad2012chesnokov@mail.ru>
2022-09-06 01:11:15 +0500

    return zip

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,12 @@ FROM python:3.9
 
 WORKDIR /usr/src/app
 
-COPY requirements.txt ./
-RUN pip install --no-cache-dir -r requirements.txt
+COPY ./requirements.txt /code/requirements.txt
 
-COPY . .
+RUN pip install --no-cache-dir --upgrade -r /code/requirements.txt
 
-CMD gunicorn -w 3 -k uvicorn.workers.UvicornWorker main:app --bind 0.0.0.0:15400
+#
+COPY ./main.py /code/
+
+#
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "80"]
--- main.py
+++ main.py
@@ -2,24 +2,24 @@ import requests
 from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
 import zipfile
-from fastapi import FastAPI, BackgroundTasks
-from fastapi.responses import FileResponse
+from flask import Flask, send_file
 import uvicorn
 import datetime
 from dataclasses import dataclass
 import xml.etree.ElementTree as ET
 
 path = 'ostatki.zip'
-app = FastAPI()
+app = Flask(__name__)
 Token = 123
 
 
-@app.get("/", response_class=FileResponse)
-async def main():
-    return path
+@app.route('/')
+def main():
+    return send_file(path)
 
 
 def update_yandex_table():
+    print('started')
     json_data = {"password": "RAMTRX1500", "regulation": True, "email": "Rakhmanov-2019@list.ru"}
     response = requests.post('https://www.sima-land.ru/api/v5/signin', json=json_data)
     token = response.json().get('token')
@@ -53,11 +53,12 @@ def update_yandex_table():
     tree.write('t.xml', encoding='utf-8')
 
 
-@app.get("/start")
-async def start(background_tasks: BackgroundTasks):
-    background_tasks.add_task(update_yandex_table)
+@app.route('/start')
+def start():
+    import threading
+    threading.Thread(target=update_yandex_table).start()
     return f"Started at {datetime.datetime.now()}"
 
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8080)
+    app.run()
