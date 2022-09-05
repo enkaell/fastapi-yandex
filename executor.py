b78fed7b17bd4941ea7e071ff733010caee1decc
vlad <vlad2012chesnokov@mail.ru>
2022-09-05 19:45:41 +0500

    return zip

--- executor.py
+++ executor.py
@@ -1,11 +1,12 @@
 import requests
 from requests.adapters import HTTPAdapter
+
 from urllib3.util.retry import Retry
 import zipfile
 import datetime
 from dataclasses import dataclass
 import xml.etree.ElementTree as ET
-
+import xml.dom.minidom as minidom
 
 path = 'yandex.xlsx'
 
@@ -23,35 +24,23 @@ def update_yandex_table():
     adapter = HTTPAdapter(max_retries=retry)
     session.mount('https://', adapter)
     for tag in root_node.findall('shop/offers/offer'):
-        try:
-            tag.remove(tag[1])
-        except Exception:
-            pass
-        try:
-            response = session.get(
-                f"https://www.sima-land.ru/api/v5/item/{tag.attrib['id']}",
-                headers={
-                    'accept': 'application/json',
-                    'X-Api-Key': token,
-                    'Authorization': token,
-                },
+        response = session.get(
+            f"https://www.sima-land.ru/api/v5/item/{tag.attrib['id']}",
+            headers={
+                'accept': 'application/json',
+                'X-Api-Key': token,
+                'Authorization': token,
+            },
 
-                params={
-                    'view': 'brief',
-                    'by_sid': 'false',
-                }
-            )
-        except Exception as e:
-            tree.write('t.xml', encoding='utf-8')
-            zf = zipfile.ZipFile("ostatki.zip", "w", compresslevel=8, compression=zipfile.ZIP_DEFLATED)
-            zf.write('t.xml', compresslevel=8)
+            params={
+                'view': 'brief',
+                'by_sid': 'false',
+            }
+        )
         if int(tag.find('count').text) < 10:
             tag.find('count').text = '0'
         else:
             tag.find('count').text = str(response.json()['balance'])
     tree.write('t.xml', encoding='utf-8')
-    zf = zipfile.ZipFile("ostatki.zip", "w", compresslevel=8, compression=zipfile.ZIP_DEFLATED)
-    zf.write('t.xml', compresslevel=8)
 
 update_yandex_table()
-print("Ended in ", datetime.datetime.now())
